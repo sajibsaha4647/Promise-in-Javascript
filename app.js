d3d1980560f46b730daea42ed474d8fff7d87fb4
sajibsaha4647 <sksaha4648@gmail.com>
2022-10-06 16:59:37 +0600

    javascript promise

--- app.js
+++ app.js
@@ -1,24 +1,64 @@
 //javascript -promise
 
-let data = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    let data = {
-      name: "bdtask",
-    };
-    resolve(data);
-  }, 0);
-});
+// let data = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     // let data = {
+//     //   name: "bdtask",
+//     // };
+//     // if (true) {
+//     //   resolve(data);
+//     // } else {
+//     //     reject("dfj")
+//     // }
+//     resolve(data);
+//     // setTimeout(() => {
+//     //   reject("issue");
+//     // }, 0);
+//     // setTimeout(() => {
+//     //   throw new Error("issue");
+//     // }, 2000);
+//   }, 0);
+// });
+
+// data
+//   .then((res) => {
+//     console.log(res, "1");
+//   })
+//   .then((ress) => {
+//     console.log(ress, "2");
+//   })
+//   .catch((r) => {
+//     console.log(r, "3");
+//   })
+//   .finally((r) => {
+//     console.log(r, "4");
+//   });
+
+let data = Promise.race([
+  new Promise((resolve, reject) => {
+    resolve("1");
+  }),
+  new Promise((resolve, reject) => {
+    resolve("2");
+  }),
+  new Promise((resolve, reject) => {
+    resolve("3");
+  }),
+]);
 
 data
   .then((res) => {
-    console.log(res, "1");
+    console.log(res);
   })
-  .then((ress) => {
-    console.log(ress, "2");
-  })
-  .catch((r) => {
-    console.log(r, "3");
-  })
-  .finally((r) => {
-    console.log(r, "4");
-  });
+  .catch((e) => {});
+
+// const firstPromise = new Promise((res, rej) => {
+//   setTimeout(res, 500, "one");
+// });
+// const secondPromise = new Promise((res, rej) => {
+//   setTimeout(res, 100, "two");
+// });
+
+// Promise.race([firstPromise, secondPromise]).then((res) => {
+//   console.log(res);
+// });
